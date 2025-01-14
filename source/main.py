6fc709bf9e39c4cd6f01573013bc01f15d26e824
Dtzz81 <denise.turkova@madetech.com>
2025-01-14 13:27:54 +0000

    code refactored (kept old one for a bit in comments)

--- source/main.py
+++ source/main.py
@@ -1,22 +1,32 @@
 def insert_dashes(num: int) -> str:
     num_to_str = str(num)
 
+    result = num_to_str[0]
 
-    for i in num_to_str:
-        if int(i) % 2 == 1:
-            if int(num_to_str[1]) % 2 == 1:
-                if len(num_to_str) < 3:
-                    return f"{i}-{num_to_str[1:]}"
-                elif int(num_to_str[2]) % 2 == 1:
-                    if len(num_to_str) < 4:
-                        return f"{i}-{num_to_str[1]}-{num_to_str[2:]}"
-                    elif int(num_to_str[3]) % 2 == 1:
-                        return f"{i}-{num_to_str[1]}-{num_to_str[2]}-{num_to_str[3:]}"
-                    else:
-                        return f"{i}-{num_to_str[1]}-{num_to_str[2:]}"
-                else:
-                    return f"{i}-{num_to_str[1:]}"
+    for i in num_to_str[1:]:  # Loop through the rest of the digits
+        if int(i) % 2 == 1 and int(result[-1]) % 2 == 1:  # Check if both the current and previous digits are odd
+            result += '-' + i 
+        else:
+            result += i
 
-        return num_to_str
+    return result
+
+
+    # for i in num_to_str[1:]:
+    #     if int(i) % 2 == 1:
+    #         if int(num_to_str[1]) % 2 == 1:
+    #             if len(num_to_str) < 3:
+    #                 return f"{i}-{num_to_str[1:]}"
+    #             elif int(num_to_str[2]) % 2 == 1:
+    #                 if len(num_to_str) < 4:
+    #                     return f"{i}-{num_to_str[1]}-{num_to_str[2:]}"
+    #                 elif int(num_to_str[3]) % 2 == 1:
+    #                     return f"{i}-{num_to_str[1]}-{num_to_str[2]}-{num_to_str[3:]}"
+    #                 else:
+    #                     return f"{i}-{num_to_str[1]}-{num_to_str[2:]}"
+    #             else:
+    #                 return f"{i}-{num_to_str[1:]}"
+
+    #     return num_to_str
 
 insert_dashes(202579)
