51f6e41b6292eaf7fcb0b361c728114d72e2c315
Dtzz81 <denise.turkova@madetech.com>
2025-01-14 09:38:22 +0000

    late comiting what I did with my LM

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,28 @@
 from source.main import insert_dashes
 
-
 def test_no_even__nor_odd_digits_next_to_each_other():
     #act
     original_digits = 45478
     #arrange
-    expected = 45478
+    expected = "45478"
+    result = insert_dashes(original_digits)
+    #assert
+    assert expected == result
+
+def test_insert_dash_between_two_odd_digits():
+    #act
+    original_digits = 79
+    #arrange
+    expected = "7-9"
+    result = insert_dashes(original_digits)
+    #assert
+    assert expected == result
+
+def test_insert_dash_between_another_two_odds():
+    #act
+    original_digits = 51
+    #arrange
+    expected = "5-1"
     result = insert_dashes(original_digits)
     #assert
     assert expected == result
