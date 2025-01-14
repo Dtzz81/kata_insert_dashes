fa8bf768e0725084a6e641f8221f6fd5067337cc
Dtzz81 <denise.turkova@madetech.com>
2025-01-14 11:08:53 +0000

    test passing

--- source/main.py
+++ source/main.py
@@ -5,10 +5,13 @@ def insert_dashes(num: int) -> str:
     for i in num_to_str:
         if int(i) % 2 == 1:
             if int(num_to_str[1]) % 2 == 1:
-                if len(num_to_str) != 3:  
+                if len(num_to_str) != 3:
                     return f"{i}-{num_to_str[1:]}"
                 elif int(num_to_str[2]) % 2 == 1:
                     return f"{i}-{num_to_str[1]}-{num_to_str[2]}"
+                else:
+                    return f"{i}-{num_to_str[1:]}"
+
         return num_to_str
 
 insert_dashes(202579)
