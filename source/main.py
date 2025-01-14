b80171a2d59b83fe56b863dfbe3fdb22ab43946a
Dtzz81 <denise.turkova@madetech.com>
2025-01-14 10:18:56 +0000

    test 3 digits (odd-eve-odd and odd-odd-even)

--- source/main.py
+++ source/main.py
@@ -4,7 +4,7 @@ def insert_dashes(num: int) -> str:
     for i in num_to_str:
         if int(i) % 2 == 1:
             if int(num_to_str[1]) % 2 == 1:
-                return f"{i}-{num_to_str[1]}"
+                return f"{i}-{num_to_str[1:]}"
 
         return num_to_str
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -10,28 +10,41 @@ def test_no_even__nor_odd_digits_next_to_each_other():
     assert expected == result
 
 def test_insert_dash_between_two_odd_digits():
-    #act
     original_digits = 79
-    #arrange
+
     expected = "7-9"
     result = insert_dashes(original_digits)
-    #assert
+
     assert expected == result
 
 def test_insert_dash_between_another_two_odds():
-    #act
     original_digits = 51
-    #arrange
+
     expected = "5-1"
     result = insert_dashes(original_digits)
-    #assert
+
     assert expected == result
 
 def test_odd_even_only():
-    #act
     original_digits = 52
-    #arrange
+
     expected = "52"
     result = insert_dashes(original_digits)
-    #assert
+
+    assert expected == result
+
+def test_three_digits_odd_even_odd():
+    original_digits = 525
+
+    expected = "525"
+    result = insert_dashes(original_digits)
+
+    assert expected == result
+
+def test_three_digits_odd_odd_even():
+    original_digits = 552
+
+    expected = "5-52"
+    result = insert_dashes(original_digits)
+
     assert expected == result
